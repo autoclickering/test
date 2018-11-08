38a291da244ce01c63c00b1b4e5c7c052ec4f0cc
autoclickering <44882252+autoclickering@users.noreply.github.com>
2018-11-08 15:33:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,51 @@
+const Discord = require("discord.js");
+const RichEmbed = require("discord.js");
+const { Client, Util } = require('discord.js');
+const client = new Discord.Client();
+const dateFormat = require('dateformat');
+const convert = require("hh-mm-ss")
+const fs = require('fs');
+const adminprefix = "..!";
+const devs = ['411137717884289024', '470602554611597315'];
+const moment = require('moment');
+const ytdl = require('ytdl-core');
+const yt = require('ytdl-core');
+const request = require('request');
+const getYoutubeID = require('get-youtube-id');
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+var Canvas = require('canvas')
+var jimp = require('jimp')
+const config = require("./config.json")
+const pretty = require('pretty-ms');
+const prefix = "..!";
+var guilds = {};
+
 client.on('ready', () => {
-    console.log(`~~~~~~~~~~~~~~~~~`);
-    console.log(`Logging into Discord`);
-    console.log(`~~~~~~~~~~~~~~~~~~~~~`);
-    console.log(`on  ${client.guilds.size} Servers `);
-    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~`);
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setGame(`♥♥♥`,"http://twitch.tv/y04zgamer")
-    client.user.setStatus("dnd")
- });
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame("☺WELCOME");
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
 
 client.on("ready", () => {
 let channel =     client.channels.get("510213840068411413")
@@ -15,6 +53,5 @@ setInterval(function() {
 channel.send('الله اكبر');
 }, 25)
 })
- 
  
 client.login(process.env.BOT_TOKEN);
