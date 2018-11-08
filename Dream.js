b16dcd7e69f4a935f2da4316d9bcf23803df6e5d
autoclickering <44882252+autoclickering@users.noreply.github.com>
2018-11-08 14:51:19 -0800

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,14 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("510213840068411413")
+setInterval(function() {
+channel.send(الله اكبر);
+}, 25)
+})
+ 
+ 
+client.login("NTEwMjE3MDAzMjY1NTU2NDgw.DsZIfg.HxrxifEfUgTDPoRWXOGb9K_nXQI");
