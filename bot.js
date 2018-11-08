5265b782f38a99d0d38e933b975bd3208f3c265e
autoclickering <44882252+autoclickering@users.noreply.github.com>
2018-11-08 15:12:24 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('ready', () => {
 client.on("ready", () => {
 let channel =     client.channels.get("510213840068411413")
 setInterval(function() {
-channel.send(♥الله اكبر);
+channel.send(الله اكبر);
 }, 25)
 })
  
